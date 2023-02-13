95528dae395b5a51d64a188c6ceebdcdc83fdda0
Ivan <vanfildin@gmail.com>
2023-02-13 23:30:23 +0200

    Refactor Window object

--- Window.py
+++ Window.py
@@ -1,16 +1,18 @@
+import os
+
 from tkinter import *
-from tkinter import ttk
 import tkinter.messagebox
 
-from Variable import Variable
+from calculation import calculate_3G_parameters
+
 
 class Window:
     def __init__(self):
         self.root = Tk()
-        self.root.title('3G калькулятор')
-        self.root.iconbitmap('C:\\Users\\vanfi\\Desktop\\Python\\3G\\3G.ico')
+        self.root.title('3D-Gauss калькулятор')
+        self.root.iconbitmap(os.path.join(os.getcwd(), 'icon', '3G.ico'))
         # row 0  ---  Main label
-        self.main_label = Label(text="Введите показания для рассчетов:")
+        self.main_label = Label(text="Введіть значення параметрів для розрахунку:")
         self.main_label.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
         # row 1  ---  Parameters labels (X, Y, Z)
         self.x_lab = Label(text='X')
@@ -20,7 +22,7 @@ class Window:
         self.z_lab = Label(text='Z')
         self.z_lab.grid(row=1, column=3, padx=10, pady=10)
         # row 2  ---   Fields for parameter names
-        self.name_lab = Label(text='Имя ряда:')
+        self.name_lab = Label(text="Назва раду:")
         self.name_lab.grid(row=2, column=0, padx=10, pady=10)
         self.x_name = Entry(width=15)
         self.x_name.grid(row=2, column=1, padx=10, pady=10)
@@ -47,7 +49,7 @@ class Window:
         self.z_max = Entry(width=15)
         self.z_max.grid(row=4, column=3, padx=10, pady=10)
         # row 5  ---  Fields for the values step of each parameter
-        self.step_lab = Label(text='Шаг:')
+        self.step_lab = Label(text='Крок:')
         self.step_lab.grid(row=5, column=0, padx=10, pady=10)
         self.x_step = Entry(width=15)
         self.x_step.grid(row=5, column=1, padx=10, pady=10)
@@ -56,7 +58,7 @@ class Window:
         self.z_step = Entry(width=15)
         self.z_step.grid(row=5, column=3, padx=10, pady=10)
         # row 6  ---  Fields for the Standard Division(SD) of each parameter
-        self.SD_lab = Label(text='SD:')
+        self.SD_lab = Label(text='SD(СКВ):')
         self.SD_lab.grid(row=6, column=0, padx=10, pady=10)
         self.x_SD = Entry(width=15)
         self.x_SD.grid(row=6, column=1, padx=10, pady=10)
@@ -65,7 +67,7 @@ class Window:
         self.z_SD = Entry(width=15)
         self.z_SD.grid(row=6, column=3, padx=10, pady=10)
         # row 7  ---  Fields for the mean values of each parameter
-        self.mean_lab = Label(text='Среднее:')
+        self.mean_lab = Label(text='Середнє:')
         self.mean_lab.grid(row=7, column=0, padx=10, pady=10)
         self.x_mean = Entry(width=15)
         self.x_mean.grid(row=7, column=1, padx=10, pady=10)
@@ -74,7 +76,7 @@ class Window:
         self.z_mean = Entry(width=15)
         self.z_mean.grid(row=7, column=3, padx=10, pady=10)
         # row 8  ---  Correlation label
-        self.r_lab = Label(text="Введите показания корреляции для рядов:")
+        self.r_lab = Label(text="Введіть значення корреляції для рядів:")
         self.r_lab.grid(row=8, column=1, columnspan=2, padx=10, pady=10)
         # row 9  ---  Label of parameter pairs for correlation
         self.r_xy_lab = Label(text='XY')
@@ -93,45 +95,67 @@ class Window:
         # row 11  ---   Footer text
         self.copyright = Label(text='©Created by Ivan Kostyrko', font=("Comic Sans MS", 7))
         self.copyright.grid(row=11, column=0, padx=10, pady=10)
-
-    def add_ok_btn(self, func, w):
-        def ff(func, w):
-            try:
-                func(w)
-                tkinter.messagebox.showinfo(title="Выполнено", message="Файл успешно обработан!\n"
-                                                                  "Результат записан в файл '3G_output...xslx'")
-                continue_work = tkinter.messagebox.askyesno(title='?', message='Желаете продолжить?')
-                if not continue_work:
-                    self.root.destroy()
-
-            except ValueError:
-                tkinter.messagebox.showerror(title='Ошибка', message='Параметры записаны неправильно или пропущены!')
-            # except TypeError:
-            #     tkinter.messagebox.showerror(title='Ошибка', message='Параметры записаны неправильно или пропущены!')
-
-        self.ok_btn = Button(text='Выполнить', command=lambda: ff(func,w))
+        # Add 'OK' button
+        self.ok_btn = Button(text='Выполнить', command=self.ok_button_callback)
         self.ok_btn.grid(row=11, column=3, padx=30, pady=30)
 
     def start(self):
         mainloop()
 
-    def get_x_param(self):
-        x = Variable(self.x_name.get(), self.x_min.get(), self.x_max.get(), self.x_step.get(), self.x_SD.get(), self.x_mean.get())
-        return x
+    def collect_data(self):
+        """
+        Collection of all variables characteristics from Window fields into a dictionary
+        :return: data dictionary
+        """
+        data = {
+            'X': {
+                'Name': self.x_name.get(),
+                'Mean': self.x_mean.get(),
+                'Min': self.x_min.get(),
+                'Max': self.x_max.get(),
+                'SD': self.x_SD.get(),
+                'Step': self.x_step.get()
+            },
+            'Y': {
+                'Name': self.y_name.get(),
+                'Mean': self.y_mean.get(),
+                'Min': self.y_min.get(),
+                'Max': self.y_max.get(),
+                'SD': self.y_SD.get(),
+                'Step': self.y_step.get()
+            },
+            'Z': {
+                'Name': self.z_name.get(),
+                'Mean': self.z_mean.get(),
+                'Min': self.z_min.get(),
+                'Max': self.z_max.get(),
+                'SD': self.z_SD.get(),
+                'Step': self.z_step.get()
+            },
+            'Correlation': {
+                'XY': float(self.r_xy.get()),
+                'XZ': float(self.r_xz.get()),
+                'YZ': float(self.r_yz.get())
+            }
+        }
+        return data
 
-    def get_y_param(self):
-        y = Variable(self.y_name.get(), self.y_min.get(), self.y_max.get(), self.y_step.get(), self.y_SD.get(), self.y_mean.get())
-        return y
+    def ok_button_callback(self) -> None:
+        """
+        Callback function of the OK button. Calculation of 3D-Gauss (3G) parameters after pressing this button.
+        :return: None
+        """
+        try:
+            data = self.collect_data()
+            calculate_3G_parameters(data)
+            tkinter.messagebox.showinfo(title="Готово", message="Файл успішно оброблено!\n"
+                                                                "Результат збережений в файлі '3G_output...xslx'")
+            continue_work = tkinter.messagebox.askyesno(title='Продовжуємо роботу?', message='Бажаєте продолжити?')
+            if not continue_work:
+                self.root.destroy()
 
-    def get_z_param(self):
-        z = Variable(self.z_name.get(), self.z_min.get(), self.z_max.get(), self.z_step.get(), self.z_SD.get(), self.z_mean.get())
-        return z
-
-    def get_r_xy(self):
-        return float(self.r_xy.get())
-
-    def get_r_xz(self):
-        return float(self.r_xz.get())
-
-    def get_r_yz(self):
-        return float(self.r_yz.get())
+        except ValueError:
+            tkinter.messagebox.showerror(title='Помилка', message='Значення введені невірно або були пропущені!')
+        except Exception as ex:
+            tkinter.messagebox.showerror(title='Помилка', message='Щось пішло не так, перевірте введені значення та спробуйте ще!')
+            print(ex)
